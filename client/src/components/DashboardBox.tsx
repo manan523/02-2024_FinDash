ce6b4f052c44bdcb925d4b492d50a926fa43d772
Yagami Light <usermananjain@gmail.com>
2024-03-27 04:07:03 +0530

    Strict True

--- client/src/components/DashboardBox.tsx
+++ client/src/components/DashboardBox.tsx
@@ -1,6 +1,6 @@
 import { Box, styled } from "@mui/material";
 
-const DashboardBox = styled(Box)(({ theme }) => ({
+const DashboardBox = styled(Box)(({theme}) => ({
   backgroundColor: theme.palette.background.light,
   borderRadius: "1rem",
   boxShadow: "0.15rem 0.2rem 0.15rem 0.1rem rgba(0,0,0,.8)",
